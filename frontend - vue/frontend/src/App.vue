95562c40c9fe707711e2a80050ad3bbc64a4fea3
kvucicevic5721rn <kvucicevic5721rn@raf.rs>
2024-06-11 21:11:45 +0200

    Added bootstrap and axios dependencies

--- frontend - vue/frontend/src/App.vue
+++ frontend - vue/frontend/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <NavigationBar/> -->
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/> 
+
+    <!-- <div class="container">
+      <router-view/>
+    </div> -->
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import NavigationBar from "./components/NavigationBar.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // NavigationBar
   }
 }
 </script>
---
+++ frontend - vue/frontend/src/router/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import HomePage from '../views/HomePage.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+//   {
+//     path: '/',
+//     name: 'HomePage',
+//     component: HomePage
+//   },
+//   {
+//     path: '/subjects',
+//     name: 'AllSubjects',
+//     meta: {
+//       authRequired: true,
+//     },
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AllSubjects.vue')
+//   },
+//   {
+//     path: '/subjects/:id',
+//     name: 'single-subject',
+//     meta: {
+//       authRequired: true,
+//     },
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/SingleSubjectPage.vue')
+//   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.authRequired) {
+    const jwt = localStorage.getItem('jwt');
+
+    if (!jwt) {
+      next({name: 'LoginPage'});
+      return;
+    }
+
+    const payload = JSON.parse(atob(jwt.split('.')[1]));
+    const expDate = new Date(payload.exp * 1000);
+    if (expDate < new Date()) {
+      next({name: 'LoginPage'});
+      return;
+    }
+  }
+
+  next();
+});
+
+export default router
